0292289e3cfdc40d2d81e0f881cd73a2913cfb75
Alex Bernt <alexbernt100@gmail.com>
2023-08-28 23:15:11 -0500

    set up tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        logoBlue: "#143763",
+        ButtonBlackHover: "#374151",
+      },
+    },
   },
   plugins: [],
 };
