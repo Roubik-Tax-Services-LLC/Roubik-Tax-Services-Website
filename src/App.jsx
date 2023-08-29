2e52857833fcd12e941f265243e952044eb9619f
Alex Bernt <alexbernt100@gmail.com>
2023-08-28 23:15:34 -0500

    set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./HomePage";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
