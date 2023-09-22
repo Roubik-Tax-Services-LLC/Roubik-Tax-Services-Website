8a0a215149e79c6be992630613d0cabe0a4bccc1
Alex Bernt <alexbernt100@gmail.com>
2023-09-21 23:32:31 -0500

    Add home page welcome

---
+++ src/components/HomePage/HomePageWelcome.jsx
@@ -0,0 +1,28 @@
+const HomePageWelcome = () => {
+  return (
+    <>
+      <div className="mx-auto flex max-w-6xl flex-col items-center justify-center px-9 py-16 sm:px-16 gap-5">
+        <h1 className="font-semibold text-3xl">
+          WELCOME TO ROUBIK TAX SERVICES LLC
+        </h1>
+        <p className="text-lg">
+          Roubik Tax Services LLC was founded to help people and families around
+          the country hold onto more of their hard-earned assets, than ever
+          before. Many folks don’t know where to turn for clear, understandable
+          tax preparation and tax planning advice that is both affordable and
+          tailored to their needs.
+        </p>
+        <p className="text-lg">
+          Roubik Tax Services LLC tax planners assist clients nationwide, giving
+          straight and honest answers to tough tax and financial questions at
+          affordable prices…so
+        </p>
+        <h4 className="font-semibold text-logoBlue text-2xl">
+          YOU Can Better Live Your American Dream!
+        </h4>
+      </div>
+    </>
+  );
+};
+
+export default HomePageWelcome;
