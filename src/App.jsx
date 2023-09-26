592dac14fbe94f7b6db615f007286fb334d61f4a
Alex Bernt <alexbernt100@gmail.com>
2023-09-25 23:17:58 -0500

    Adjust routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import HomePage from "./HomePage";
 import About from "./About";
 import Services from "./Services";
 import Questionaire from "./Questionaire";
-import Reviews from "./Reviews";
 import Footer from "./components/Footer";
 
 function NotFound() {
@@ -33,7 +32,6 @@ function App() {
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/about" element={<About />} />
         <Route exact path="/services" element={<Services />} />
-        <Route exact path="/reviews" element={<Reviews />} />
         <Route exact path="/questionaire" element={<Questionaire />} />
         <Route exact path="*" element={<NotFound />} />
       </Routes>
