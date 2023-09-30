e52ea24d8650971f8420b2e3cd2d11be28c19c59
Alex Bernt <alexbernt100@gmail.com>
2023-09-30 15:17:35 -0500

    change phone number

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -7,12 +7,12 @@ const ContactInfo = () => {
     <div className="flex w-full flex-col items-center justify-center gap-3 bg-gray-200 py-6 text-sm sm:flex-row sm:gap-7 md:gap-16 md:text-base lg:gap-32">
       <div className="flex items-center gap-2 font-bold">
         <img src={phone} alt="Phone icon" className="w-6" />
-        <a href="tel:+14536780978">(453)678-0978</a>
+        <a href="tel:+14536780978">414-316-6006</a>
       </div>
       <div className="flex items-center gap-2 font-bold">
         <img src={email} alt="Email icon" className="w-7" />
         <a href="mailto:joe@roubiktaxservices.com">
-          <p>joe@roubiktaxservices.com</p>
+          <p>info@roubiktax.com</p>
         </a>
       </div>
       <div className="flex items-center gap-2 font-bold">
