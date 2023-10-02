ab85aac8260f03deece65c4828e8829ea11a791b
Alex Bernt <alexbernt100@gmail.com>
2023-10-02 13:16:40 -0500

    Change routes

--- src/TaxPrep.jsx
+++ src/TaxPrep.jsx
@@ -7,7 +7,7 @@ import { HashLoader } from "react-spinners";
 import { scrollToTop } from "./utils/scrollToTop";
 import { useEffect, useState } from "react";
 
-const TaxPrep = () => {
+const TaxPreparation = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -47,63 +47,244 @@ const TaxPrep = () => {
       ) : (
         <>
           <ContactInfo />
-
-            
-
-            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-              <div className="grid gap-5 row-gap-8 lg:grid-cols-2">
-                <div className="flex flex-col justify-center">
-                  <div className="max-w-xl mb-6">
-                    <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-                      Tax Preparation
-                    </h2>
-                    <p className="text-base text-gray-700 md:text-lg">
-                      So perhaps you're considering a new tax pro this year.
-                      Most tax professionals do a fine job putting the "right"
-                      numbers in the "right" boxes on the "right" forms, but
-                      then they call it a day. It doesn't matter how good your
-                      tax preparer is with a stack of receipts on April 15. The
-                      real secret to reducing your tax bill is planning.
+          <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+            <div className="grid gap-6 row-gap-10 lg:grid-cols-2">
+              <div className="relative">
+                <img
+                  className="inset-0 object-cover object-bottom w-full rounded shadow-lg h-96 lg:absolute lg:h-full"
+                  src={taxPlanning}
+                  alt=""
+                />
+              </div>
+              <div className="lg:py-6 lg:pr-16">
+                <div className="max-w-xl mb-10 md:mx-auto lg:max-w-2xl md:mb-12">
+                  <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+                    Tax Preparation
+                  </h2>
+                  <p className="text-base text-gray-700 md:text-lg">
+                    We can prepare and file your State and Federal Income Tax
+                    Returns regardless of your location within the continental
+                    USA.
+                  </p>
+                </div>
+                <div className="flex">
+                  <div className="flex flex-col items-center mr-4">
+                    <div>
+                      <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                        <svg
+                          className="w-4 text-gray-600"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          viewBox="0 0 24 24"
+                        >
+                          <line
+                            fill="none"
+                            strokeMiterlimit="10"
+                            x1="12"
+                            y1="2"
+                            x2="12"
+                            y2="22"
+                          />
+                          <polyline
+                            fill="none"
+                            strokeMiterlimit="10"
+                            points="19,15 12,22 5,15"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="w-px h-full bg-gray-300" />
+                  </div>
+                  <div className="pt-1 pb-8">
+                    <p className="mb-2 text-lg font-bold">Step 1</p>
+                    <p className="text-gray-700">
+                      Complete our online client questionnaire.
                     </p>
                   </div>
-                  <div className="grid gap-5 row-gap-8 sm:grid-cols-2">
-                    <div className="bg-white border-l-4 shadow-sm border-deep-purple-accent-400">
-                      <div className="h-full p-5 border border-l-0 rounded-r">
-                        <h6 className="mb-2 font-semibold leading-5">
-                          At Roubik Tax Services, we're different.
-                        </h6>
-                        <p className="text-sm text-gray-900">
-                          We don't just record history. We help you write it
-                          with a complete lineup of concepts and strategies to
-                          give you the savings you really want.
-                        </p>
+                </div>
+                <div className="flex">
+                  <div className="flex flex-col items-center mr-4">
+                    <div>
+                      <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                        <svg
+                          className="w-4 text-gray-600"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          viewBox="0 0 24 24"
+                        >
+                          <line
+                            fill="none"
+                            strokeMiterlimit="10"
+                            x1="12"
+                            y1="2"
+                            x2="12"
+                            y2="22"
+                          />
+                          <polyline
+                            fill="none"
+                            strokeMiterlimit="10"
+                            points="19,15 12,22 5,15"
+                          />
+                        </svg>
                       </div>
                     </div>
-                    <div className="bg-white border-l-4 shadow-sm border-deep-purple-accent-400">
-                      <div className="h-full p-5 border border-l-0 rounded-r">
-                        <h6 className="mb-2 font-semibold leading-5">
-                          This year, don't settle for just "filing" your return.
-                        </h6>
-                        <p className="text-sm text-gray-900">
-                          We'll find the mistakes and missed opportunities that
-                          may be costing you thousands and show you how
-                          proactive tax planning can save those dollars.
-                        </p>
+                    <div className="w-px h-full bg-gray-300" />
+                  </div>
+                  <div className="pt-1 pb-8">
+                    <p className="mb-2 text-lg font-bold">Step 2</p>
+                    <p className="text-gray-700">
+                      Create an account and login to our Secure Portal. Submit
+                      this year's important tax documents (or US Mail, fax, or
+                      locally by appointment).
+                    </p>
+                  </div>
+                </div>
+                <div className="flex">
+                  <div className="flex flex-col items-center mr-4">
+                    <div>
+                      <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                        <svg
+                          className="w-4 text-gray-600"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          viewBox="0 0 24 24"
+                        >
+                          <line
+                            fill="none"
+                            strokeMiterlimit="10"
+                            x1="12"
+                            y1="2"
+                            x2="12"
+                            y2="22"
+                          />
+                          <polyline
+                            fill="none"
+                            strokeMiterlimit="10"
+                            points="19,15 12,22 5,15"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="w-px h-full bg-gray-300" />
+                  </div>
+                  <div className="pt-1 pb-8">
+                    <p className="mb-2 text-lg font-bold">Step 3</p>
+                    <p className="text-gray-700">
+                      Call (453) 678-0978 to schedule FREE client consultation
+                      and review of documents.
+                    </p>
+                  </div>
+                </div>
+                <div className="flex">
+                  <div className="flex flex-col items-center mr-4">
+                    <div>
+                      <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                        <svg
+                          className="w-4 text-gray-600"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          viewBox="0 0 24 24"
+                        >
+                          <line
+                            fill="none"
+                            strokeMiterlimit="10"
+                            x1="12"
+                            y1="2"
+                            x2="12"
+                            y2="22"
+                          />
+                          <polyline
+                            fill="none"
+                            strokeMiterlimit="10"
+                            points="19,15 12,22 5,15"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="w-px h-full bg-gray-300" />
+                  </div>
+                  <div className="pt-1 pb-8">
+                    <p className="mb-2 text-lg font-bold">Step 4</p>
+                    <p className="text-gray-700">
+                      We prepare and review your tax return (Direct calls to you
+                      for clarification, if necessary).
+                    </p>
+                  </div>
+                </div>
+                <div className="flex">
+                  <div className="flex flex-col items-center mr-4">
+                    <div>
+                      <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                        <svg
+                          className="w-4 text-gray-600"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          viewBox="0 0 24 24"
+                        >
+                          <line
+                            fill="none"
+                            strokeMiterlimit="10"
+                            x1="12"
+                            y1="2"
+                            x2="12"
+                            y2="22"
+                          />
+                          <polyline
+                            fill="none"
+                            strokeMiterlimit="10"
+                            points="19,15 12,22 5,15"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="w-px h-full bg-gray-300" />
+                  </div>
+                  <div className="pt-1 pb-8">
+                    <p className="mb-2 text-lg font-bold">Step 5</p>
+                    <p className="text-gray-700">
+                      We deliver and review your completed tax returns (Secure
+                      Portal, Mail, or locally by appointment)
+                    </p>
+                  </div>
+                </div>
+                <div className="flex">
+                  <div className="flex flex-col items-center mr-4">
+                    <div>
+                      <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                        <svg
+                          className="w-6 text-gray-600"
+                          stroke="currentColor"
+                          viewBox="0 0 24 24"
+                        >
+                          <polyline
+                            fill="none"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeMiterlimit="10"
+                            points="6,12 10,16 18,8"
+                          />
+                        </svg>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div>
-                  <img
-                    className="object-cover w-full h-56 rounded shadow-lg sm:h-96"
-                    src={taxPreparation}
-                    alt=""
-                  />
+                  <div className="pt-1">
+                    <p className="mb-2 text-lg font-bold">Success</p>
+                    <p className="text-gray-700" />
+                  </div>
                 </div>
               </div>
-            
-
-            
+            </div>
             <div className="mt-12 justify-center md:flex lg:gap-10 md:gap-4 md:flex-row">
               <ClientQuestionnaire />
               <ClientPortal />
@@ -115,4 +296,4 @@ const TaxPrep = () => {
   );
 };
 
-export default TaxPrep;
+export default TaxPreparation;
